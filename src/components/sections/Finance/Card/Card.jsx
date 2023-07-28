02b2dc5ca88f8c7c3d139ddfce114301a7fd99a1
Josenilson Farias <fariaslwork@gmail.com>
2023-07-28 14:49:20 -0300

    styles: responsive

--- src/components/sections/Finance/Card/Card.jsx
+++ src/components/sections/Finance/Card/Card.jsx
@@ -4,12 +4,16 @@ export const Card =({description, value, type, uuid, deleteMoneyList })=>{
     return (
         <li >
             <div className={(type==0 ? style.entrada : style.saida)}>
-                <h2 className="title two">{description}</h2>
-                <p className="paragraph">{(type == 0 ? "Entrada" : "Saida")}</p>
-                <p className="title two">{value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL',})}</p>
-                <img src={trash} alt="apagar" onClick={(event)=>{
-                    deleteMoneyList(uuid)
-                }}/>
+                <div>
+                    <h2 className="title two">{description}</h2>
+                    <p className="paragraph">{(type == 0 ? "Entrada" : "Saida")}</p>
+                </div>
+                <div>
+                    <p className="title two">{value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL',})}</p>
+                    <img src={trash} alt="apagar" onClick={(event)=>{
+                        deleteMoneyList(uuid)
+                    }}/>
+                </div>
             </div>
         </li>
     )
